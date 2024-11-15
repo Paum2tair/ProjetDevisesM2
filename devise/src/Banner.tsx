741dc4ae73850b0140799d4e51f3362fc7fb952f
Emie18 <lalala.emiliat@gmail.com>
2024-11-15 11:55:12 +0100

    modified:   devise/src/Banner.tsx 	modified:   devise/src/style/banner.css

--- devise/src/Banner.tsx
+++ devise/src/Banner.tsx
@@ -12,7 +12,12 @@ const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
         <div className="banner-container" >
             <h1 className="banner-title">{title}</h1>
             {subtitle && <p className="banner-subtitle">{subtitle}</p>}
-            <a href="/exchange-man" className="home_bt">J’en profite</a>
+            <a href="/exchange-man" className="home_bt">
+            <span className="text">J’en profite</span>
+            <span class="fleche zero">&gt;</span>
+            <span class="fleche un">&gt;</span>
+            <span class="fleche deux">&gt;</span>
+            </a>
         </div>
     );
 };
